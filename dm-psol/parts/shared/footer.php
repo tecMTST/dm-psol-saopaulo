9dba8c0638050a879a52d27ecac8af7072246e8a
Vitor Norton <vtnorton@outlook.com>
2023-08-18 18:58:32 -0300

    Redes sociais

--- dm-psol/parts/shared/footer.php
+++ dm-psol/parts/shared/footer.php
@@ -3,19 +3,19 @@
 		<img src="<?php echo get_template_directory_uri(); ?>/img/footer-logo.png" alt="Logo PSOL">
 		<div class="links">
 			<div class="social">
-				<a href="https://www.youtube.com/@ABIMAPIBRASIL" class="social-icon si-small h-bg-youtube">
+				<a href="https://www.youtube.com/" class="social-icon si-small h-bg-youtube">
 					<i class="fa-brands fa-youtube"></i>
 				</a>
-				<a href="https://www.linkedin.com/company/abimapi-international/" class="social-icon si-small h-bg-facebook">
+				<a href="https://www.linkedin.com/company/" class="social-icon si-small h-bg-facebook">
 					<i class="fa-brands fa-facebook-f"></i>
 				</a>
-				<a href="https://twitter.com/Abimapi_inter" class="social-icon si-small me-0 h-bg-linkedin">
+				<a href="https://twitter.com/" class="social-icon si-small me-0 h-bg-linkedin">
 					<i class="fa-brands fa-twitter"></i>
 				</a>
-				<a href="https://www.instagram.com/abimapi.inter/" class="social-icon si-small me-0 h-bg-linkedin">
+				<a href="https://www.instagram.com/" class="social-icon si-small me-0 h-bg-linkedin">
 					<i class="fa-brands fa-instagram"></i>
 				</a>
-				<a href="https://www.linkedin.com/company/abimapi-international/" class="social-icon si-small me-0 h-bg-linkedin">
+				<a href="https://www.linkedin.com/company/" class="social-icon si-small me-0 h-bg-linkedin">
 					<i class="fa-brands fa-linkedin"></i>
 				</a>
 			</div>
