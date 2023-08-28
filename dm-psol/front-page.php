97681f5bcad59e556e49f91f226c43d3e78efb64
Daniel Silva <devmaquina.ds@gmail.com>
2023-08-28 04:20:12 -0300

    Implementa seção de notícias

--- dm-psol/front-page.php
+++ dm-psol/front-page.php
@@ -24,9 +24,67 @@
 		</div>
 	</section>
 	<section class="noticias">
-		
-	</section>
 		<h2>NOTÍCIAS</h2>
+		<div class="row">
+			<div class="col-md-3">
+				<div class="card-noticias">
+					<div class="thumbnail-noticias" style="background:url(<?php echo get_template_directory_uri() ?>/img/boulos.jpg);background-size: cover;">
+					</div>
+					<div class="content-card">
+						<div class="title-news">
+							<h3>Lorem ipsum</h3>
+						</div>
+						<div class="excerpt">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="card-noticias">
+					<div class="thumbnail-noticias" style="background:url(<?php echo get_template_directory_uri() ?>/img/boulos.jpg);background-size: cover;">
+					</div>
+					<div class="content-card">
+						<div class="title-news">
+							<h3>Lorem ipsum</h3>
+						</div>
+						<div class="excerpt">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="card-noticias">
+					<div class="thumbnail-noticias" style="background:url(<?php echo get_template_directory_uri() ?>/img/boulos.jpg);background-size: cover;">
+					</div>
+					<div class="content-card">
+						<div class="title-news">
+							<h3>Lorem ipsum</h3>
+						</div>
+						<div class="excerpt">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="card-noticias">
+					<div class="thumbnail-noticias" style="background:url(<?php echo get_template_directory_uri() ?>/img/boulos.jpg);background-size: cover;">
+					</div>
+					<div class="content-card">
+						<div class="title-news">
+							<h3>Lorem ipsum</h3>
+						</div>
+						<div class="excerpt">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+		
 	<section>
 		<h2>VEREADORES</h2>
 		<div class="vereadores">
