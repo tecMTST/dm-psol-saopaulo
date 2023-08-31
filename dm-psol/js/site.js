93ce588ba39be67c87961295fa343441c2c91512
Daniel Silva <79273132+dsilvadori@users.noreply.github.com>
2023-08-31 18:05:49 -0300

    Feature/carrousel (#16)
    * Implementa a area de carrossel do topo da home
    
    * Implementa seção de notícias
    
    * Implementa banner congresso
    
    ---------
    
    Co-authored-by: Daniel Silva <devmaquina.ds@gmail.com>

--- dm-psol/js/site.js
+++ dm-psol/js/site.js
@@ -5,3 +5,33 @@ jQuery(document).ready(function($) {
 
 });
 
+const swiperBanners = function(){
+	return swiper = new Swiper('.swiper', {
+	  // Default parameters
+	  slidesPerView: 1,
+	  spaceBetween: 10,
+	  speed: 500,
+	  autoplay: {
+		delay: 4000,
+	  },
+	  navigation: {
+		  nextEl: '.swiper-button-next',
+		  prevEl: '.swiper-button-prev',
+	  },
+	  pagination: {
+		el: '.swiper-pagination',
+		type: 'bullets',
+		clickable: true,
+	  }, 
+	  // Responsive breakpoints
+	  breakpoints: {
+		// when window width is >= 640px
+		640: {
+		  slidesPerView: 1,
+		  spaceBetween: 10,
+		},   
+	  }
+	})
+  }
+  
+  swiperBanners();
