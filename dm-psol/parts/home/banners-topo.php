844236511648336913c75f74e002b8b967d6672e
Daniel Silva <devmaquina.ds@gmail.com>
2023-10-14 06:15:04 -0300

    Ajustes formulários

--- dm-psol/parts/home/banners-topo.php
+++ dm-psol/parts/home/banners-topo.php
@@ -1,5 +1,8 @@
     <section class="topo">
 		<div class="container">
-			<a href="#" target="_blank" rel="noopener noreferrer"><img src="https://placehold.co/1920x700" alt=""></a>
+			<a href="#" target="_blank" rel="noopener noreferrer">
+				<img class="only-desktop" src="https://placehold.co/1920x700" alt="">
+				<img class="only-mobile" src="https://placehold.co/780x600" alt="">
+			</a>
 		</div>
 	</section>
