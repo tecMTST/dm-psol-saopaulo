5751ff8a62911c2fe73e202dd4798fd064fd432a
Daniel Silva <devmaquina.ds@gmail.com>
2023-10-24 22:02:32 -0300

    Ajustes de campos personalizados

--- dm-psol/parts/home/banners-topo.php
+++ dm-psol/parts/home/banners-topo.php
@@ -1,6 +1,6 @@
     <section class="topo">
 		<div class="container">
-		<?php if( have_rows('banner_principal', 'option') ): ?>
+		<?php if( have_rows('banner_principal', 'option') ): // Campos personalizados ?>
 			<?php while( have_rows('banner_principal', 'option') ): the_row(); ?>
 			<a href="<?php the_sub_field('link_do_banner'); ?>">
 				<img class="only-desktop" src="<?php the_sub_field('banner_desktop'); ?>" alt="Banner principal">
