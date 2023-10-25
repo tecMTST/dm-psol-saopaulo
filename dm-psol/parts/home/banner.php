d659b6e64acd731c5f1399f906629395d2d213a0
Daniel Silva <devmaquina.ds@gmail.com>
2023-10-24 21:48:14 -0300

    Ajuste para edição de banners

--- dm-psol/parts/home/banner.php
+++ dm-psol/parts/home/banner.php
@@ -1,10 +1,12 @@
-    <section class="banner-d">
-        <div class="container">
-            <div class="banner">
-                <a href="#" target="_blank" rel="noopener noreferrer">
-                    <img class="only-desktop" src="https://placehold.co/1920x500" alt="">
-                    <img class="only-mobile" src="https://placehold.co/780x600" alt="">
-                </a>
-            </div>
-        </div>
+	<section class="banner-d">
+		<div class="container">
+		<?php if( have_rows('banner_central', 'option') ): ?>
+			<?php while( have_rows('banner_central', 'option') ): the_row(); ?>
+			<a href="<?php the_sub_field('link_do_banner'); ?>">
+				<img class="only-desktop" src="<?php the_sub_field('banner_desktop'); ?>" alt="Banner principal">
+				<img class="only-mobile" src="<?php the_sub_field('banner_mobile'); ?>" alt="Banner principal">
+			</a>
+			<?php endwhile; ?>
+		<?php endif; ?>
+		</div>
 	</section>
--- dm-psol/parts/home/banners-topo.php
+++ dm-psol/parts/home/banners-topo.php
@@ -1,8 +1,12 @@
     <section class="topo">
 		<div class="container">
-			<a href="#" target="_blank" rel="noopener noreferrer">
-				<img class="only-desktop" src="https://placehold.co/1920x700" alt="">
-				<img class="only-mobile" src="https://placehold.co/780x600" alt="">
+		<?php if( have_rows('banner_principal', 'option') ): ?>
+			<?php while( have_rows('banner_principal', 'option') ): the_row(); ?>
+			<a href="<?php the_sub_field('link_do_banner'); ?>">
+				<img class="only-desktop" src="<?php the_sub_field('banner_desktop'); ?>" alt="Banner principal">
+				<img class="only-mobile" src="<?php the_sub_field('banner_mobile'); ?>" alt="Banner principal">
 			</a>
+			<?php endwhile; ?>
+		<?php endif; ?>
 		</div>
 	</section>
